2c49d91ac9d16cce8b8dff19a48bbd5e902127d3
arina <trepacheva.arina@yandex.ru>
2024-11-30 17:04:54 +0300

    add design from class(Design)

--- main.py
+++ main.py
@@ -1,15 +1,77 @@
 import sys
 from random import randrange
+import io
 
 from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 
+template = '''<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>767</width>
+    <height>644</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="pushButton">
+    <property name="geometry">
+     <rect>
+      <x>230</x>
+      <y>20</y>
+      <width>251</width>
+      <height>61</height>
+     </rect>
+    </property>
+    <property name="styleSheet">
+     <string notr="true">font-size: 25px;
+background-color: rgb(255, 252, 228);
+border-radius:20px;</string>
+    </property>
+    <property name="text">
+     <string>Нажать </string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>767</width>
+     <height>26</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+'''
+
+class Design(QWidget):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.init_ui(parent)
+
+    def init_ui(self, parent):
+        f = io.StringIO(template)
+        uic.loadUi(f, parent)
+
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        design = Design(self)
         self.pushButton.clicked.connect(self.paint)
         self.do_paint = False
 
